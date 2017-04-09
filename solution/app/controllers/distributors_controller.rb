e0e9019b420c66452db37196b63ceedced55c489
Suganyashree <suganya.gc@gmail.com>
2017-04-09 13:33:40 +0530

    Done with permission check

--- solution/app/controllers/distributors_controller.rb
+++ solution/app/controllers/distributors_controller.rb
@@ -18,9 +18,13 @@ class DistributorsController < ApplicationController
   def check
     @distributor = Hash.new
 
-    unless params["distributor"].nil? || params["distributor"].empty?
+    unless params["distributor_id"].nil? || params["distributor_id"].empty?
       unless params["country"].nil? || params["country"].empty?
-        @message = check_permission
+        if check_permission(params["distributor_id"], params["country"], params["province"], params["city"])
+          @message = "The distributor has permission for this location"
+        else
+          @message = "The distributor does not have permission for this location"
+        end
       else
         @message = "Please enter a location to check permission!"
       end
@@ -87,8 +91,50 @@ class DistributorsController < ApplicationController
       @locations = CSV.read(Rails.root.join('db', 'cities.csv'), :headers=>true)
     end
 
-    def check_permission
-      return "The distributor has/doesn't have permissions to ditribute in this location!"
+    def check_permission(distributor_id, country, province, city)
+      # Check for the current distributor exclusion
+      excluded = check_exclusion(distributor_id, country, province, city)
+      return false if excluded
+
+      traverse_path = []
+      traverse_path.push(distributor_id)
+      # else check until parent reached
+      parent_id = @distributors[distributor_id]['parent_id']
+      until parent_id.empty?
+        return false if check_exclusion(parent_id, country, province, city)
+        traverse_path.push(parent_id)
+        parent_id = @distributors[parent_id]['parent_id']
+      end
+
+      traverse_path.reverse.each do |dist_id|
+        included = check_inclusion(dist_id, country, province, city)
+        return false unless included
+      end
+      return true
+      # included = check_exclusion(distributor_id, country, province, city)
+      # return true if included
+    end
+
+    def check_exclusion(distributor_id, country, province, city)
+      excluded_locations = @distributors[distributor_id]['exclude']
+
+      excluded_locations.each do |location|
+        if location =~ /^#{country}$/ || location =~ /^#{province}_#{country}$/ || location =~ /^#{city}_#{province}_#{country}$/
+          return true
+        end
+      end
+      return false
+    end
+
+    def check_inclusion(distributor_id, country, province, city)
+      included_locations = @distributors[distributor_id]['include']
+
+      included_locations.each do |location|
+        if location =~ /^#{country}$/ || location =~ /^#{province}_#{country}$/ || location =~ /^#{city}_#{province}_#{country}$/
+          return true
+        end
+      end
+      return false
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
