8a5d402046c7ce3525df7acb83ac6c0affaa242f
srebalaji <srebalaji@outlook.com>
2017-04-17 16:41:28 +0530

    Adding files to run the app

--- distributor_manager/lib/create_places.rb
+++ distributor_manager/lib/create_places.rb
@@ -1,6 +1,6 @@
 require 'csv'
 require 'json'
-cities = CSV.read("cities.csv");
+cities = CSV.read("../app/assets/csv/cities.csv");
 json = []
 
 cities.each_with_index do |city, index|
@@ -10,6 +10,10 @@ cities.each_with_index do |city, index|
 	country = cities[index][2]
 	state = cities[index][1]
 	city = cities[index][0]
+
+	country_name = cities[index][5]
+	state_name = cities[index][4]
+	city_name = cities[index][3]
 	
 	json.each do |j|
 		if j['code'] == country
@@ -21,6 +25,7 @@ cities.each_with_index do |city, index|
 					add_city = Hash.new
 					add_city["id"] = state_id.to_s+"."+cities_count.to_s
 					add_city["code"] = city
+					add_city["name"] = city_name
 					child['children'] << add_city
 					skip_add_state = 1
 					skip_add_country = 1
@@ -36,11 +41,13 @@ cities.each_with_index do |city, index|
 				add_state = Hash.new
 				add_state["id"] = state_id
 				add_state["code"] = state
+				add_state["name"] = state_name
 				add_state["children"] = []
 
 				add_city = Hash.new
 				add_city["id"] = state_id.to_s+"."+1.to_s
 				add_city["code"] = city
+				add_city["name"] = city_name
 
 				temp_city = []
 				temp_city << add_city
@@ -57,16 +64,19 @@ cities.each_with_index do |city, index|
 		country_count = json.count + 1
 		add_country["id"] = country_count.to_s
 		add_country["code"] = country
+		add_country["name"] = country_name
 		add_country["children"] = []
 
 		add_state = Hash.new
 		add_state["id"] = country_count.to_s+"."+1.to_s
 		add_state["code"] = state
+		add_state["name"] = state_name
 		add_state["children"] = []
 
 		add_city = Hash.new
 		add_city["id"] = country_count.to_s+"."+1.to_s+"."+1.to_s
 		add_city["code"] = city
+		add_city["name"] = city_name
 		
 		temp_city = []
 		temp_city << add_city
@@ -77,7 +87,7 @@ cities.each_with_index do |city, index|
 		add_country["children"] = temp_state
 
 		json << add_country
-		puts country
+		puts country_name
 	end
 end
 
