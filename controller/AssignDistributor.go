9d35be1778ba342890b7301919f76578f1950392
syedmoosa <bsyed6@gmail.com>
2018-10-31 15:05:25 +0530

    Added Validation for assigning permissions for distributor

--- controller/AssignDistributor.go
+++ controller/AssignDistributor.go
@@ -30,49 +30,122 @@ func (d *AssignDistributor) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			log.Fatal(err)
 		}
 
-		if distributor, ok := d.Distributors[permission.For]; ok {
-			for _, v := range permission.Includes {
-				isPresent := false
-				for _, element := range distributor.Includes {
-					if v == element {
-						isPresent = true
-					}
-				}
-				if !isPresent {
-					distributor.Includes = append(distributor.Includes, v)
-				}
-			}
+		//Validating if the country is present in our countries datastore
+		for _, country := range permission.Includes {
+			if provinces, ok := d.Countries[country]; ok {
+				// Checking if the Distributor is already present in the distributor datastore
+				if distributor, ok := d.Distributors[permission.For]; ok {
 
-			for _, v := range permission.Excludes {
-				isPresent := false
-				for _, element := range distributor.Excludes {
-					if v.Province == element.Province && element.City == "ALL" {
-						isPresent = true
-					} else if v.Province == element.Province && element.City == v.City {
-						isPresent = true
+					for _, v := range permission.Includes {
+						isPresent := false
+						for _, element := range distributor.Includes {
+							if v == element {
+								isPresent = true
+							}
+						}
+						if !isPresent {
+							distributor.Includes = append(distributor.Includes, v)
+						}
 					}
-				}
-				if !isPresent {
-					if v.City == "" {
-						v.City = "ALL"
+
+					for _, e := range permission.Excludes {
+
+						// check e.Country is present in existing distributor.Includes
+						isCountryPresent := false
+						for _, existingCountry := range distributor.Includes {
+							if existingCountry == e.Country {
+								isCountryPresent = true
+							}
+						}
+						// check e.Country is present in existing permission.Includes
+
+						for _, newCountry := range permission.Includes {
+							if newCountry == e.Country {
+								isCountryPresent = true
+							}
+						}
+
+						if isCountryPresent {
+							// checking v.Province is present in  Provinces(countries datastore)
+							for _, p := range provinces {
+								if e.Province == p {
+									isPresent := false
+									for _, element := range distributor.Excludes {
+										if e.Province == element.Province && element.City == "ALL" {
+											isPresent = true
+										} else if e.Province == element.Province && element.City == e.City {
+											isPresent = true
+										}
+									}
+									if !isPresent {
+										if e.City == "" {
+											e.City = "ALL"
+											distributor.Excludes = append(distributor.Excludes, e)
+										} else {
+
+											// checking if the city is present in that province
+											for _, c := range d.Cities[p] {
+												if e.City == c {
+													distributor.Excludes = append(distributor.Excludes, e)
+
+												}
+											}
+										}
+
+									}
+
+								}
+							}
+						}
 					}
-					distributor.Excludes = append(distributor.Excludes, v)
+					d.Distributors[permission.For] = distributor
+					fmt.Println(d.Distributors)
 
-				}
-			}
+				} else {
+					var newPermission model.Permission
+					for _, country := range permission.Includes {
+						if provinces, ok := d.Countries[country]; ok {
+							// distributor.Includes = permission.Includes
+							newPermission.Includes = append(newPermission.Includes, country)
+							for _, e := range permission.Excludes {
+								isCountryPresent := false
+								for _, newCountry := range permission.Includes {
+									if newCountry == e.Country {
+										isCountryPresent = true
+									}
+								}
+								if isCountryPresent {
 
-			fmt.Println(distributor)
-		} else {
-			Exclude := []model.Excluded{}
-			for _, v := range permission.Excludes {
-				if v.City == "" {
-					v.City = "ALL"
+									// checking v.Province is present in  Provinces(countries datastore)
+									for _, p := range provinces {
+										if e.Province == p {
+											for _, c := range d.Cities[p] {
+
+												if e.City == c {
+													newPermission.Excludes = append(newPermission.Excludes, e)
+												}
+											}
+											if e.City == "" {
+												e.City = "ALL"
+												newPermission.Excludes = append(newPermission.Excludes, e)
+
+											}
+										}
+									}
+								}
+							}
+							d.Distributors[permission.For] = newPermission
+
+						}
+
+					}
+
+					fmt.Println(d.Distributors)
 				}
-				Exclude = append(Exclude, v)
+
+			} else {
+				return
 			}
-			permission.Excludes = Exclude
-			d.Distributors[permission.For] = permission
-			fmt.Println(d.Distributors)
 		}
 
 	}
