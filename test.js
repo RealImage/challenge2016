6ca25757e627f4f1712eb0e3d993dde699cf5349
Lakshmipriya Mukundan <lakshmipriya.m@cubettech.com>
2019-06-13 18:53:48 +0530

    changes

---
+++ test.js
@@ -0,0 +1,11 @@
+const checkFn = (exArray, str) => {
+  return exArray.includes(str);
+};
+
+const test = (include, exclude, qsn) => {
+  const ins = ["IN", "US"];
+  const exc = ["KA-IN", "CH-TN-IN"];
+
+  const qsnStr = "SE-TN-IN";
+  const qsnArray = [""];
+};
