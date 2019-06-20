cbd8c1b79db5b054215f5c8956deb4e6b2a16cfa
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2019-06-20 06:21:51 +0530

    changes

--- test.js
+++ test.js
@@ -7,5 +7,20 @@ const test = (include, exclude, qsn) => {
   const exc = ["KA-IN", "CH-TN-IN"];
 
   const qsnStr = "SE-TN-IN";
-  const qsnArray = [""];
+  const qsnArray = ["SE-TN-IN", "TN-IN", "IN"];
+
+  const inEx = (ele) => {
+    return exc.includes(ele);
+  }
+  const inIn = (ele) => {
+    return ins.includes(ele);
+  }
+
+  console.log(qsnArray.some(inEx))
+  console.log(qsnArray.some(inIn))
 };
+
+
+const saveDistributor = ()
+
+////test();
