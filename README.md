db3b5219ac654ca5080b109378ef6e7f630b5288
RohithER12 <rohither12@outlook.com>
2023-09-18 12:12:18 +0530

    implimented necessery features

--- README.md
+++ README.md
@@ -1,41 +1,70 @@
-# Real Image Challenge 2016
+Distributor Permission System
+Overview
 
-In the cinema business, a feature film is usually provided to a regional distributor based on a contract for exhibition in a particular geographical territory.
+The Distributor Permission System is an open-source Go project designed to manage distributor permissions and distribution rights based on geographical regions (city, state, country). This project is a valuable addition to your portfolio, demonstrating your coding skills and problem-solving abilities.
+Features
+1. Permission Management
 
-Each authorization is specified by a combination of included and excluded regions. For example, a distributor might be authorzied in the following manner:
-```
-Permissions for DISTRIBUTOR1
-INCLUDE: INDIA
-INCLUDE: UNITEDSTATES
-EXCLUDE: KARNATAKA-INDIA
-EXCLUDE: CHENNAI-TAMILNADU-INDIA
-```
-This allows `DISTRIBUTOR1` to distribute in any city inside the United States and India, *except* cities in the state of Karnataka (in India) and the city of Chennai (in Tamil Nadu, India).
+Easily configure permissions for distributors:
 
-At this point, asking your program if `DISTRIBUTOR1` has permission to distribute in `CHICAGO-ILLINOIS-UNITEDSTATES` should get `YES` as the answer, and asking if distribution can happen in `CHENNAI-TAMILNADU-INDIA` should of course be `NO`. Asking if distribution is possible in `BANGALORE-KARNATAKA-INDIA` should also be `NO`, because the whole state of Karnataka has been excluded.
+    Include: Specify regions (city, state, country) where a distributor is authorized to distribute.
+    Exclude: Define regions to exclude from a distributor's distribution rights.
 
-Sometimes, a distributor might split the work of distribution amount smaller sub-distiributors inside their authorized geographies. For instance, `DISTRIBUTOR1` might assign the following permissions to `DISTRIBUTOR2`:
+2. Hierarchical Structure
 
-```
-Permissions for DISTRIBUTOR2 < DISTRIBUTOR1
-INCLUDE: INDIA
-EXCLUDE: TAMILNADU-INDIA
-```
-Now, `DISTRIBUTOR2` can distribute the movie anywhere in `INDIA`, except inside `TAMILNADU-INDIA` and `KARNATAKA-INDIA` - `DISTRIBUTOR2`'s permissions are always a subset of `DISTRIBUTOR1`'s permissions. It's impossible/invalid for `DISTRIBUTOR2` to have `INCLUDE: CHINA`, for example, because `DISTRIBUTOR1` isn't authorized to do that in the first place. 
+Create a hierarchical structure of distributors:
 
-If `DISTRIBUTOR2` authorizes `DISTRIBUTOR3` to handle just the city of Hubli, Karnataka, India, for example:
-```
-Permissions for DISTRIBUTOR3 < DISTRIBUTOR2 < DISTRIBUTOR1
-INCLUDE: HUBLI-KARNATAKA-INDIA
-```
-Again, `DISTRIBUTOR2` cannot authorize `DISTRIBUTOR3` with a region that they themselves do not have access to. 
+    Add sub-distributors to existing distributors, allowing for complex distribution networks.
 
-We've provided a CSV with the list of all countries, states and cities in the world that we know of - please use the data mentioned there for this program. *The codes you see there may be different from what you see here, so please always use the codes in the CSV*. This Readme is only an example. 
+3. Location-Based Authorization
 
-Write a program in any language you want (If you're here from Gophercon, use Go :D) that does this. Feel free to make your own input and output format / command line tool / GUI / Webservice / whatever you want. Feel free to hold the dataset in whatever structure you want, but try not to use external databases - as far as possible stick to your langauage without bringing in MySQL/Postgres/MongoDB/Redis/Etc.
+The system checks authorization based on location data, providing distributors with precise distribution rights.
+4. Interactive CLI
 
-To submit a solution, fork this repo and send a Pull Request on Github. 
+The interactive command-line interface (CLI) makes it simple to:
 
-For any questions or clarifications, raise an issue on this repo and we'll answer your questions as fast as we can.
+    Add new distributors.
+    Configure permissions.
+    Check distributions.
+    Add sub-distributors.
 
+Getting Started
 
+Follow these steps to run the program and start using it:
+
+    Ensure you have Go installed on your system.
+
+    Clone the repository:
+
+    bash
+
+git clone https://github.com/RohithER12/challenge_Qube_cinimas.git
+cd distributor-permission-system
+
+Compile and run the program:
+
+bash
+
+    go run main.go
+
+    Use the interactive menu to perform various actions and explore the features.
+
+Usage
+Adding a New Distributor
+
+    Easily add new distributors by providing their names.
+    Prevent duplicate entries with a built-in check.
+
+Configuring Permissions
+
+    Set precise permissions by including or excluding specific regions (city, state, country).
+    The system checks parent distributor permissions for sub-distributors.
+
+Checking Distributions
+
+    Verify if a distributor is authorized to distribute in a particular region.
+    Get detailed distribution information based on location.
+
+Adding Sub-Distributors
+
+    Expand your distribution network by adding sub-distributors to existing distributors.
---
+++ main.go
@@ -0,0 +1,441 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Permission struct {
+	include []string
+	exclude []string
+}
+
+type Location struct {
+	City    string
+	State   string
+	Country string
+}
+
+type Distributor struct {
+	name            string
+	permission      Permission
+	subDistributors []Distributor
+}
+
+var DistributerData map[string]Distributor
+var PlacesMap map[string]Location
+var subDistsByName map[string]string
+
+func (p *Permission) IsAuthorized(region string, type_req string) bool {
+	location, found := PlacesMap[region]
+	new_reg1 := ""
+	new_reg2 := ""
+	new_reg3 := ""
+	if !found {
+		fmt.Println("Location not found")
+	} else {
+		if type_req == "city" {
+			new_reg1 = region + "-" + location.State + "-" + location.Country
+			new_reg2 = location.State + "-" + location.Country
+			new_reg3 = location.Country
+		} else if type_req == "state" {
+			new_reg1 = location.State + "-" + location.Country
+			new_reg2 = location.Country
+			new_reg3 = location.Country
+		} else if type_req == "country" {
+			new_reg1 = location.Country
+			new_reg2 = location.Country
+			new_reg3 = location.Country
+		}
+	}
+	for _, e := range p.exclude {
+		if strings.HasPrefix(new_reg1, e) || strings.HasPrefix(new_reg2, e) || strings.HasPrefix(new_reg3, e) {
+			return false
+		}
+	}
+
+	for _, i := range p.include {
+		if strings.HasPrefix(new_reg1, i) || strings.HasPrefix(new_reg2, i) || strings.HasPrefix(new_reg3, i) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (d *Distributor) IsAuthorized(region string, type_req string) bool {
+	if d.permission.IsAuthorized(region, type_req) {
+		return true
+	}
+
+	return false
+}
+
+func main() {
+	InitPlacesData()
+main_menu:
+	fmt.Println("Distributer List : ", DistributerData)
+	fmt.Println("Select Options \n 1. Add new Distributor \n 2. Add Permission \n 3. Check distibutions \n 4. Add SubDistributors")
+	arg := 0
+	fmt.Scan(&arg)
+	scanner := bufio.NewScanner(os.Stdin)
+	switch arg {
+	case 1:
+		fmt.Println("Enter Distributer's Name: ")
+		name := ""
+		scanner.Scan()
+		name = scanner.Text()
+		fmt.Println("name", name)
+		name = strings.ReplaceAll(strings.ToLower(name), " ", "_")
+		_, ok := DistributerData[name]
+		if ok {
+			fmt.Println("Distributer already exit ")
+			goto main_menu
+		} else {
+
+			distributer := &Distributor{
+				name:            name,
+				permission:      Permission{},
+				subDistributors: []Distributor{},
+			}
+			DistributerData[name] = *distributer
+			include_things := []string{}
+			exclude_things := []string{}
+		permissionOpt:
+			fmt.Println("Adding Permissions..\n  1. Include \n 2. Exclude \n 3. Main menu")
+			permissions := 0
+			fmt.Scan(&permissions)
+			switch permissions {
+			case 1, 2:
+				fmt.Println("Enter Country name(enter nil if not needed) : ")
+				country, state, city := "", "", ""
+
+				scanner.Scan()
+				country = scanner.Text()
+
+				if country != "nil" && country != "" {
+					fmt.Println("Enter State name(enter nil if not needed) :  ")
+
+					scanner.Scan()
+					state = scanner.Text()
+					if state != "nil" && state != "" {
+						fmt.Println("Enter City name(enter nil if not needed) : ")
+
+						scanner.Scan()
+						city = scanner.Text()
+					}
+				}
+				include_perm := ""
+				if (country != "nil" && country != "") || (state != "nil" && state != "") || (city != "nil" && city != "") {
+					if country != "nil" && country != "" {
+						include_perm += strings.ReplaceAll(strings.ToLower(country), " ", "_")
+					}
+					if state != "nil" && state != "" {
+						include_perm = "-" + include_perm
+						include_perm = strings.ReplaceAll(strings.ToLower(state), " ", "_") + include_perm
+					}
+					if city != "nil" && city != "" {
+						include_perm = "-" + include_perm
+						include_perm = strings.ReplaceAll(strings.ToLower(city), " ", "_") + include_perm
+					}
+				}
+				if permissions == 1 {
+					include_things = append(include_things, include_perm)
+				} else {
+					exclude_things = append(exclude_things, include_perm)
+				}
+
+				goto permissionOpt
+			case 3:
+				distributer.permission.include = include_things
+				distributer.permission.exclude = exclude_things
+				DistributerData[name] = *distributer
+				goto main_menu
+			}
+		}
+
+	case 2:
+	add_perm:
+		fmt.Println("Enter Distributer/Sub Distributor's  Name: ")
+		name := ""
+
+		scanner.Scan()
+		name = scanner.Text()
+		fmt.Println("name", name)
+		name = strings.ReplaceAll(strings.ToLower(name), " ", "_")
+		var sub bool
+		data, ok := DistributerData[name]
+		dataa := Distributor{}
+		if !ok {
+			if para, ex := subDistsByName[name]; ex {
+				dataa, ok = DistributerData[para]
+				if ok {
+					ok, data = isSubDistributorOf(dataa, name)
+					sub = true
+
+				}
+			}
+
+		}
+		if ok {
+			include_things := []string{}
+			exclude_things := []string{}
+		permissionOpt2:
+			fmt.Println("Adding Permissions..\n 1. Include \n 2. Exclude \n 3. Main menu")
+			permissions := 0
+			fmt.Scan(&permissions)
+			switch permissions {
+			case 1, 2:
+				fmt.Println("Enter Country name(enter nil if not needed) : ")
+				country, state, city := "nil", "nil", "nil"
+
+				scanner.Scan()
+				country = scanner.Text()
+
+				if country != "nil" && country != "" {
+					country = strings.ReplaceAll(strings.ToLower(country), " ", "_")
+					fmt.Println("Enter State name(enter nil if not needed) :  ")
+
+					scanner.Scan()
+					state = scanner.Text()
+					if state != "nil" && state != "" {
+						state = strings.ReplaceAll(strings.ToLower(state), " ", "_")
+						fmt.Println("Enter City name(enter nil if not needed) : ")
+
+						scanner.Scan()
+						city = scanner.Text()
+						if city != "nil" && state != "" {
+							city = strings.ReplaceAll(strings.ToLower(city), " ", "_")
+						}
+					}
+				}
+				include_perm := ""
+				auth := false
+				if (country != "nil" && country != "") || (state != "nil" && state != "") || (city != "nil" && city != "") {
+					if sub {
+						if city != "nil" && city != "" {
+							auth = dataa.IsAuthorized(city, "city")
+						} else if state != "nil" && state != "" {
+							auth = dataa.IsAuthorized(state, "state")
+
+						} else if country != "nil" && country != "" {
+							auth = dataa.IsAuthorized(country, "country")
+
+						}
+					}
+					if !sub || auth {
+						if country != "nil" && country != "" {
+							include_perm += country
+						}
+						if state != "nil" && state != "" {
+							include_perm = "-" + include_perm
+							include_perm = state + include_perm
+						}
+						if city != "nil" && city != "" {
+							include_perm = "-" + include_perm
+							include_perm = city + include_perm
+						}
+					}
+				}
+				if permissions == 1 {
+					include_things = append(include_things, include_perm)
+				} else {
+					exclude_things = append(exclude_things, include_perm)
+				}
+
+				if sub && !auth {
+					fmt.Println("You Parent dont have the access in this place \n.")
+				}
+
+				goto permissionOpt2
+			case 3:
+				data.permission.include = append(data.permission.include, include_things...)
+				data.permission.exclude = append(data.permission.exclude, exclude_things...)
+				if sub {
+					dataa.subDistributors = []Distributor{}
+					dataa.subDistributors = append(dataa.subDistributors, data)
+					DistributerData[strings.ToLower(dataa.name)] = dataa
+				} else {
+					DistributerData[strings.ToLower(name)] = data
+				}
+				goto main_menu
+			}
+		} else {
+			fmt.Println("Enter valid Distributer \n.")
+			goto add_perm
+		}
+
+	case 3:
+	check_new_dist:
+		fmt.Println("Enter Distributer/Sub Distributor's Name to check  the permission: \n.")
+		name := ""
+
+		scanner.Scan()
+		name = scanner.Text()
+		name = strings.ReplaceAll(strings.ToLower(name), " ", "_")
+
+		data, ok := DistributerData[strings.ToLower(name)]
+
+		if !ok {
+			if para, ex := subDistsByName[name]; ex {
+				dataa, okk := DistributerData[para]
+				if okk {
+					ok, data = isSubDistributorOf(dataa, name)
+				}
+			}
+
+		}
+
+		if ok {
+		check_dist:
+			fmt.Println("\nSearch by \n ")
+			fmt.Println(" 1. City \n 2. State \n 3. Country  \n 4. Check again \n 5. Main menu\n.")
+			input := 0
+			fmt.Scan(&input)
+			region := ""
+			if input == 1 || input == 2 || input == 3 {
+				fmt.Println("Enter the Place \n.")
+
+				scanner.Scan()
+				region = scanner.Text()
+
+				region = strings.ReplaceAll(strings.ToLower(region), " ", "_")
+			}
+
+			type_req := ""
+			switch input {
+			case 1:
+				type_req = "city"
+			case 2:
+				type_req = "state"
+			case 3:
+				type_req = "country"
+			case 4:
+				goto check_dist
+			case 5:
+				goto main_menu
+			default:
+				fmt.Println("Enter Valid input")
+				goto check_dist
+
+			}
+			auth := data.IsAuthorized(region, type_req)
+			if auth {
+				fmt.Printf("%s Can Distribute in %s", data.name, region)
+			} else {
+				fmt.Printf("%s Can't Distribute in %s", data.name, region)
+
+			}
+			goto check_dist
+
+		} else {
+			fmt.Println("\n Enter valid Distributer")
+			goto check_new_dist
+		}
+
+	case 4:
+		fmt.Println("Enter Parent Distributer's Name  ")
+		name := ""
+		scanner.Scan()
+		name = scanner.Text()
+		name = strings.ReplaceAll(strings.ToLower(name), " ", "_")
+		// sub := false
+		data, ok := DistributerData[strings.ToLower(name)]
+
+		if ok {
+			fmt.Println("Enter sub Distributer's Name: ")
+			sub_name := ""
+
+			scanner.Scan()
+			sub_name = scanner.Text()
+			fmt.Println("sub_name", sub_name)
+			sub_name = strings.ReplaceAll(strings.ToLower(sub_name), " ", "_")
+
+			Value, _ := isSubDistributorOf(data, sub_name)
+			if Value {
+				fmt.Println("Sub Distributer is already there")
+
+			} else {
+				distributer := Distributor{
+					name:            sub_name,
+					permission:      Permission{},
+					subDistributors: []Distributor{},
+				}
+				if subDistsByName == nil {
+					subDistsByName = make(map[string]string)
+				}
+				subDistsByName[sub_name] = name
+
+				data.subDistributors = append(data.subDistributors, distributer)
+				DistributerData[name] = data
+
+			}
+		} else {
+			fmt.Println("NO distributer Found")
+		}
+		goto main_menu
+	}
+
+}
+
+func InitPlacesData() {
+	DistributerData = make(map[string]Distributor, 0)
+	sub_dist1 := Distributor{
+		name:            "Dist2",
+		permission:      Permission{},
+		subDistributors: []Distributor{},
+	}
+	distributer := &Distributor{
+		name:            "Dist1",
+		permission:      Permission{include: []string{"kerala-india"}, exclude: []string{"japan"}},
+		subDistributors: []Distributor{sub_dist1},
+	}
+
+	DistributerData["dist1"] = *distributer
+
+	subDistsByName = make(map[string]string)
+	subDistsByName["dist2"] = "dist1"
+
+	file, err := os.Open("cities.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	data, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	// Create data structure for quick lookups
+	if PlacesMap == nil {
+		PlacesMap = make(map[string]Location)
+	}
+	for _, row := range data {
+		location := Location{
+			City:    strings.ReplaceAll(strings.ToLower(row[3]), " ", "_"),
+			State:   strings.ReplaceAll(strings.ToLower(row[4]), " ", "_"),
+			Country: strings.ReplaceAll(strings.ToLower(row[5]), " ", "_"),
+		}
+		PlacesMap[location.City] = location
+		PlacesMap[location.State] = location
+		PlacesMap[location.Country] = location
+	}
+}
+
+func isSubDistributorOf(parentDist Distributor, subDistName string) (bool, Distributor) {
+	if parentDist.name == subDistName {
+		return true, Distributor{}
+	}
+	for _, subDist := range parentDist.subDistributors {
+		if subDist.name == subDistName {
+			return true, subDist
+		}
+	}
+	return false, Distributor{}
+}
